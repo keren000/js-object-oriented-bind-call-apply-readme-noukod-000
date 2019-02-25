f49dc3346deefb067b30665853bbc9b2d601ce91
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-25 17:17:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ class Event {
         this.keywords = keywords;
     }
 }
-
+ 
 class User {
     constructor(name, interests) {
         this.name = name;
         this.interests = interests;
     }
-
+ 
     matchInterests(event) {
-        return event.keywords.some(function(word) {
-            return this.interests.includes(word);
-        });
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
     }
 }
-
+ 
 let billy = new User('billy', ['music', 'art', 'movies']);
 let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
-
+ 
 billy.matchInterests(freeMusic);
-// Uncaught TypeError: Cannot read property 'interests' of undefined
